84062cb9a9a424e1346a2c7abfe6474a32ddef2f
Wendy Kelly <wendykelly@Wendys-iMac.local>
2023-02-04 18:14:21 -0800

    add font

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import localFont from '@next/font/local'
 
 const inter = Inter({ subsets: ['latin'] })
-
+const myFont = localFont({ src: '../fonts/savate-regular-webfont.woff' })
 export default function Home() {
   return (
     <>
@@ -14,7 +15,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
+      <main className={myFont.className}>
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
