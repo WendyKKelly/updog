ea73a811634b539159a006bf89b75ad868cb6c26
Wendy Kelly <wendykelly@Wendys-iMac.local>
2023-02-06 18:35:44 -0800

    formatting

--- pages/buy.tsx
+++ pages/buy.tsx
@@ -23,14 +23,14 @@ export default function Home() {
         <h5>
             Treats are baked weekly in small batches.</h5>
 
-            <h1>Products:</h1> 
+            <h5>Products:</h5> 
 <ul><li>
             120 grams dog treats: $10</li>
-            <li>cute upcycled treat pouch: $10</li>
+            <li>upcycled treat pouch: $10</li>
 
             <li>refills in treat pouch: $9 </li></ul>
- <h1>How To Pay</h1>
-            <p>In the short term, simply text Wendy at 236-972-7673. I will deliver your treats to you. You can then etransfer the money to us. </p>
+ <h5>How To Pay:</h5>
+            <p>For now, simply text Wendy at 236-972-7673. I will deliver your treats to you. You can then etransfer the money to us. </p>
 
             <p>This is our first iteration. We welcome your feedback!
             We are committed to uplifting and upcycling so treats are packaged in upcycled packaging sourced locally.
