8e717373e32611d2dbef16439b2e2c9ba8adbb8c
Wendy Kelly <wendykelly@Wendys-iMac.local>
2023-02-07 17:35:17 -0800

    formatting 4 products

--- components/buy.tsx
+++ components/buy.tsx
@@ -19,89 +19,94 @@ export default function Buy() {
       </Head>
       <section className={styles.section}>
       <main className={styles.main}>
-      
       <div className={styles.grid2}>
 
-<div className={styles.center2}>
+     <div className={styles.center2}>
         <h5 className={cormorant.className}>
             Treats are baked weekly in small batches.</h5>
-
-            <h5 className={cormorant.className}>Products:</h5> 
+            <h5 className={cormorant.className}>Products:</h5>
+            </div>
             <div className={styles.grid}>
               <div className={styles.card}>
-          <Image 
-            src="/images/120-grams.png"
-            alt="Dog Treat"
-            
-            width={300}
-            height={300}
-            
-         / >
-          <div className={styles.desc}>
-            <h4 className={grandstander.className}>
-              Upcylcled Treat Pouch <span>-&gt;</span>
+            <Image
+                src="/images/120-grams.png"
+                alt="Dog Treats"
+                className={styles.center1}
+                width={300}
+                height={300}
+                priority
+              />
+<h4 className={grandstander.className}>
+              Dog Treats <span>-&gt;</span>
             </h4>
             <p className={grandstander.className}>
-              Lightweight, closable, carabiner attached to loop.
-          </p>
-          </div>
-          </div>
-
-          <div className={styles.card}>
-          <Image 
-            src="/images/120-grams.png"
-            alt="Dog Treat"
-            
-            width={300}
-            height={300}
-            
-         / >
-            <div className={styles.desc}>
-            <h4 className={grandstander.className}>
-              What's Spent Grain? <span>-&gt;</span>
-            </h4>
-            <p className={grandstander.className}>
-              It's the barley grain left after brewers use it to make beer. Yes, it's super healthy for dogs (and humans!)
+              120 grams 
+              $10
             </p>
             </div>
-          </div>
-
-          <div className={styles.card}>
-          <Image 
-            src="/images/120-grams.png"
-            alt="Dog Treat"
-            
-            width={300}
-            height={300}
-            
-         / >
-          <div className={styles.desc}>
-            <h4 className={grandstander.className}>
-              What's Our Story? <span>-&gt;</span>
+            <div className={styles.card}>
+            <Image
+                src="/images/120-grams.png"
+                alt="Dog Treats"
+                className={styles.center1}
+                width={300}
+                height={300}
+                priority
+              />
+<h4 className={grandstander.className}>
+              Dog Treats <span>-&gt;</span>
             </h4>
             <p className={grandstander.className}>
-              We're a couple of dog lovers who wanted to feed our dog healthy, fresh, locally made treats.
+              120 grams 
+              $10
             </p>
-          </div>
-
-          <div className={styles.card}>
-          <Image 
-            src="/images/120-grams.png"
-            alt="Dog Treat"
-            
-            width={300}
-            height={300}
-            
-         / >
-          <div className={styles.desc}>
-            <h4 className={grandstander.className}>
-              What's The Deal? <span>-&gt;</span>
+            </div>
+            <div className={styles.card}>
+            <Image
+                src="/images/120-grams.png"
+                alt="Dog Treats"
+                className={styles.center1}
+                width={300}
+                height={300}
+                priority
+              />
+<h4 className={grandstander.className}>
+              Dog Treats <span>-&gt;</span>
             </h4>
             <p className={grandstander.className}>
-              $10 for 120 grams of treats. $10 for a handmade, upcycled treat pouch. Buy both and treat refills are $9 for 120 grams. 
+              120 grams 
+              $10
             </p>
+            </div>
+            <div className={styles.card}>
+            <Image
+                src="/images/120-grams.png"
+                alt="Dog Treats"
+                className={styles.center1}
+                width={300}
+                height={300}
+                priority
+              />
+<h4 className={grandstander.className}>
+              Dog Treats <span>-&gt;</span>
+            </h4>
+            <p className={grandstander.className}>
+              120 grams 
+              $10
+            </p>
+            </div>
+
+
+
+              
+              </div>
+              
+            
           
-        </div>
+           
+         
+         
+         
  <h5 className={cormorant.className}>How To Pay:</h5>
             <p >For now, simply text Wendy at 236-972-7673. I will deliver your treats to you. You can then etransfer the money to us. </p>
 
@@ -110,10 +115,7 @@ export default function Buy() {
             We encourage you to purchase an upcycled dog treat bag and refill it.</p>
 
 </div>
-</div>
 
-
-       
         </main>
         </section>
         </>
