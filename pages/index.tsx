a9c6744959b8b119863ec18e0800a3c222193b5f
Wendy Kelly <wendykelly@Wendys-iMac.local>
2023-02-12 12:12:45 -0800

    start seo - og, desc, google console tag in DNS

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,16 @@ export default function Home() {
     <>
       <Head>
         <title>Updog Treats</title>
-        <meta name="description" content="hyper local healthy dog treats" />
+        <meta name="description" content="Fresh healthy dog treats made in Nelson British Columbia: Updog Treats — Uplift and Upcycle" />
+        <meta property="og:title" content="Updog Treats: Nelson's Healthy Dog Treats" />
+        <meta
+          property="og:description"
+          content="Fresh, healthy, locally made in Nelson BC — Updog Treats"
+        />
+        <meta
+          property="og:image"
+          content="/images/updoglogogold.png"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
