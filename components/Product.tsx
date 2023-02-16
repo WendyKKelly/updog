75c936bdf4b2308c3c7268817f115585a38b0d6b
Wendy Kelly <wendykelly@Wendys-iMac.local>
2023-02-16 10:28:59 -0800

    fonts on product sec

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,6 +1,9 @@
 import Image from 'next/image'
 import styles from '../styles/Product.module.css';
-
+import { Grandstander } from '@next/font/google'
+import {Cormorant} from '@next/font/google'
+const grandstander = Grandstander({ subsets: ['latin'] })
+const cormorant = Cormorant({subsets: ['latin']})
 export interface IProduct {
     id: string
     name: string
@@ -18,8 +21,10 @@ const Product = (props: IProductProps) => {
     return (
         <section>
         <div className={styles.product}>
-            <h5 className={styles.title}>{props.product.name}</h5>
-            <p className={styles.description}>{props.product.description}</p>
+            <div className={grandstander.className}>
+            <h5 className={styles.title}>{props.product.name}</h5></div>
+            <div className={cormorant.className}>
+            <p className={styles.description}>{props.product.description}</p></div>
             <div className={styles.image}>
             <Image 
             src={props.product.image} 
@@ -28,7 +33,7 @@ const Product = (props: IProductProps) => {
             height={300}
             priority />
             </div>
-            <div className="product__price-button-container">
+            <div className="product__price-button-container"><div className={cormorant.className}>
                 <div className={styles.price}>${props.product.price.toFixed(2)}</div>
                 <button
                     className={`snipcart-add-item ${styles.button}`}
@@ -39,6 +44,7 @@ const Product = (props: IProductProps) => {
                     data-item-image={props.product.image.src}>
                     Add to cart
                 </button>
+                </div>
             </div>
         </div>
         </section>
