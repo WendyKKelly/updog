faf6a3c1dcae68b76a0f0e2c4c51c24ce07c2a0e
Wendy Kelly <wendykelly@Wendys-iMac.local>
2023-02-16 17:17:14 -0800

    add insta

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -3,6 +3,7 @@ import { FaInstagram } from 'react-icons/fa';
 import { Grandstander } from '@next/font/google'
 import {Cormorant} from '@next/font/google'
 import styles from './Footer.module.css';
+import Link from 'next/link';
 const grandstander = Grandstander({ subsets: ['latin'] })
 const cormorant = Cormorant({subsets: ['latin']})
 const Footer = ({ children }) => {
@@ -29,12 +30,19 @@ const Footer = ({ children }) => {
             </h4>
             
         </div>
+        <Link
+            href="https://www.instagram.com/updogtreats/"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
         <div className={styles.card}>
         <h4 className={styles.insta}>
               <FaInstagram /> <span>-&gt;</span>
             </h4>
            
         </div>
+        </Link>
         </div>
       </Container>
     </footer>
