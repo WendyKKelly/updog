fc6e8c109fe1d75a5a927d57b9ecfab47273d46d
Wendy Kelly <wendykelly@Wendys-iMac.local>
2023-02-18 14:58:42 -0800

    more copy

--- pages/nutrition.tsx
+++ pages/nutrition.tsx
@@ -48,7 +48,7 @@ export default function Nutrition() {
     </ul>
 
     <h5 className={cormorant.className}>
-            The thing is, though, that these treats taste <span className={styles.pink}>GOOD</span>. Dogs love them and have been known to wait paitiently for <span className={styles.pink}>minutes</span>just to get another. Think of all the training gains...think of all the love ...
+            Better <span className={styles.pink}>(?)</span> than nutrition, these treats taste <span className={styles.pink}>GOOD</span>. Dogs love them and have been known to wait paitiently for <span className={styles.pink}>minutes</span> just to get another. Think of all the training gains...think of all the love ...
           </h5>
     </div>
     
