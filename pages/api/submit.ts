37c2462f02288376e07f9a56c620501da1b03bcf
Wendy Kelly <wendykelly@Wendys-iMac.local>
2023-02-19 09:05:49 -0800

    add new id

--- pages/api/submit.ts
+++ pages/api/submit.ts
@@ -22,7 +22,9 @@ export default async function handler(
         const auth = new google.auth.GoogleAuth({
             credentials: {
                 client_email: process.env.GOOGLE_CLIENT_EMAIL,
-                private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n')
+                private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+                client_id:process.env.
+                GOOGLE_CLIENT_ID
             },
             scopes: [
                 'https://www.googleapis.com/auth/drive',
