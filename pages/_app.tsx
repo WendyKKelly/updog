afa4e4035da0afd3b0c007b8a17672d3e234366e
Wendy Kelly <wendykelly@Wendys-iMac.local>
2023-02-19 14:07:41 -0800

    in deep with this chakra thing...

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,14 @@
 import '@/styles/globals.scss'
-
+import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 
 
 
  export default function MyApp({ Component, pageProps }: AppProps) {
   return <div className="app">
-    
+    <ChakraProvider>
   <Component {...pageProps} />
+  </ChakraProvider>
   </div>
  }
 
