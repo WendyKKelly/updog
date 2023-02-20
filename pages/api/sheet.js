d01f4737b54732345aca0afea64d3e2717facba3
Wendy Kelly <wendykelly@Wendys-iMac.local>
2023-02-20 13:42:07 -0800

    try try again

--- pages/api/sheet.js
+++ pages/api/sheet.js
@@ -1,8 +1,35 @@
+import { GoogleApis } from "googleapis";
+
 async function handler (req, res) {
     if (req.method === "POST"){
-    		const {name, message} = req.body;
+    		const name = "Wendy";
 		res.json({message: "It still works!"});
     }
-}
 
+const auth = new google.auth.GoogleAuth({
+    credentials: {
+      client_email: process.env.CLIENT_EMAIL,
+      client_id: process.env.CLIENT_ID,
+      private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+    },
+    scopes: [
+      'https://www.googleapis.com/auth/drive',
+      'https://www.googleapis.com/auth/drive.file',
+      'https://www.googleapis.com/auth/spreadsheets',
+    ],
+  });
+  const sheets = google.sheets({
+    auth,
+    version: 'v4',
+  });
+  const response = await sheets.spreadsheets.values.append({
+    spreadsheetId: process.env.DATABASE_ID,
+    range: 'Sheet1!A2:C',
+    valueInputOption: 'USER_ENTERED',
+    requestBody: {
+      values: [["name"]],
+    },
+  });
+}
+  res.status(201).json({response, result: "Feedback posted to spreadsheet!"})
 export default handler;
