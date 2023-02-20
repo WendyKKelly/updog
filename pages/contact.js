865fc430eb730b48969ebc9c59f42acab28fc433
Wendy Kelly <wendykelly@Wendys-iMac.local>
2023-02-20 09:39:04 -0800

    typo in contact

--- pages/contact.js
+++ pages/contact.js
@@ -43,7 +43,7 @@ export default function Contact() {
                 mt={2}
                 {...register('Phone', { required: 'Please enter your phone' })}
               />
-              {errorsPhone && errors.Phone.message}
+              {errors.Phone && errors.Phone.message}
               <Input
                 placeholder="Enter Message"
                 variant="filled"
