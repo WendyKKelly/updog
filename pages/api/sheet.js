9e5e3421fc6ffe6ebe8a61c5224f4e8a3dd39548
Wendy Kelly <wendykelly@Wendys-iMac.local>
2023-02-20 13:02:18 -0800

    trying yet again

---
+++ pages/api/sheet.js
@@ -0,0 +1,5 @@
+function handler (req, res) {
+	res.json({message: "It works!"});
+}
+
+export default handler;
---
+++ pages/form.js
@@ -0,0 +1,9 @@
+async function submitHandler (data) {
+	const response = await fetch("/api/sheet", {
+    		method: "POST",
+        	body: JSON.stringify(data),
+        	headers: {
+        		'Content-Type': 'application/json',
+      		},
+    	})
+}
