b7e01b7c03aa6e68dc39e69b4b140be197c65c5f
Wendy Kelly <wendykelly@Wendys-iMac.local>
2023-02-20 09:25:00 -0800

    three checks in place

--- pages/api/submit.js
+++ pages/api/submit.js
@@ -1,4 +1,4 @@
-
+import {google} from "googleapis"
 
 
 
@@ -8,6 +8,37 @@ export default function handler(
 ) {
   return res.status(200).json({message: "It works!"})
 }
+async function handler (req, res) {
+  if (req.method === "POST"){
+      const {name, message, phone} = req.body;
+  res.json({message: "It works?"});
+  }
+}
+const auth = new google.auth.GoogleAuth({
+  credentials: {
+    client_email: process.env.CLIENT_EMAIL,
+    client_id: process.env.CLIENT_ID,
+    private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+  },
+  scopes: [
+    'https://www.googleapis.com/auth/drive',
+    'https://www.googleapis.com/auth/drive.file',
+    'https://www.googleapis.com/auth/spreadsheets',
+  ],
+});
+const sheets = google.sheets({
+  auth,
+  version: 'v4',
+});
+const response = await sheets.spreadsheets.values.append({
+  spreadsheetId: process.env.SPREADSHEET_ID,
+  range: 'Sheet1!A1:C1',
+  valueInputOption: 'USER_ENTERED',
+  requestBody: {
+    values: [[name, message, phone]],
+  },
+});
+res.status(201).json({response, result: "Feedback posted to spreadsheet!"})
 
 
 
