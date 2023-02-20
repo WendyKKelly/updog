f2cb0e6e88f5f1b778f088a5b355a1b046d83482
Wendy Kelly <wendykelly@Wendys-iMac.local>
2023-02-20 10:50:26 -0800

    now literally strip down to bare form

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import styles from '../styles/Contact.module.css';
 
-import { VStack, Stack, Text, Input, Button, Flex } from '@chakra-ui/react';
+
 import { useForm } from 'react-hook-form';
 
 export default function Contact() {
@@ -50,46 +50,17 @@ export default function Contact() {
       </Head>
 
       <main className={styles.main}>
-        <VStack spacing="1px" width="70%" align="center">
-          <Text fontSize="2xl" fontWeight="bold">
-            Your response matters!
-          </Text>
+      <form onSubmit={handleSubmit}>
+      <label htmlFor="first">First Name</label>
+      <input type="text" id="first" name="first" required />
 
-          <Stack textAlign={'center'} flexDirection={'column'}>
-            <form onSubmit={handleSubmit}>
-              <Input
-                placeholder="Enter Name"
-                variant="filled"
-                mt={2}
-                
-              />
+      <label htmlFor="last">Last Name</label>
+      <input type="text" id="last" name="last" required />
+
+      <button type="submit">Submit</button>
+    </form>
+  
               
-              <Input
-                placeholder="Enter Message"
-                variant="filled"
-                mt={2}
-               
-              />
-         
-              <VStack align="center">
-                <Button
-                  colorScheme="teal"
-                  bg={'gray.500'}
-                  textColor={'white'}
-                  _hover={{
-                    bgColor: 'pink.200',
-                    textColor: 'gray.900',
-                  }}
-                  type="submit"
-                  mt={2}
-                  variant="ghost"
-                >
-                  Submit Form
-                </Button>
-              </VStack>
-            </form>
-          </Stack>
-        </VStack>
       </main>
     </div>
   );
