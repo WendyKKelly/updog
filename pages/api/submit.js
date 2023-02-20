7aa93294645ba870fb7dab5760e6ac6543a4ca93
Wendy Kelly <wendykelly@Wendys-iMac.local>
2023-02-20 09:33:20 -0800

    found error in submit

--- pages/api/submit.js
+++ pages/api/submit.js
@@ -4,7 +4,7 @@ import {google} from "googleapis"
 
 async function handler (req, res) {
   if (req.method === "POST"){
-      const {name, message, phone} = req.body;
+      const phone, message} = req.body;
   res.json({message: "It works?"});
   }
 }
@@ -29,7 +29,7 @@ const response = await sheets.spreadsheets.values.append({
   range: 'Sheet1!A1:C1',
   valueInputOption: 'USER_ENTERED',
   requestBody: {
-    values: [[name, message, phone]],
+    values: [[phone, message]],
   },
 });
 res.status(201).json({response, result: "Feedback posted to spreadsheet!"})
