ed978df846bd9288286e7517a8a2b30325563d74
Wendy Kelly <wendykelly@Wendys-iMac.local>
2023-02-21 14:26:58 -0800

    got rid of quotes

--- pages/contact.js
+++ pages/contact.js
@@ -11,7 +11,7 @@ return (
 <div className="max-w-xs my-2 overflow-hidden rounded shadow-lg">
   <div className="px-6 py-4">
     <div className="mb-2 text-xl font-bold">Contact us</div>
-    <form className="flex flex-col"onSubmit="{submitContact}">
+    <form onSubmit={submitContact}>
       <label htmlFor="name" className="mb-2 italic">Name</label>
       <input
         className="mb-4 border-b-2"
