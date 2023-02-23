12b0e9525d5c7796593fa83a7ace41a1493eca66
Wendy Kelly <wendykelly@Wendys-iMac.local>
2023-02-23 07:15:04 -0800

    replace fix

--- pages/contact.js
+++ pages/contact.js
@@ -3,12 +3,11 @@
 import React, { useState } from 'react';
 import { GoogleSpreadsheet } from 'google-spreadsheet';
 
+
 // Config variables
-const SPREADSHEET_ID = process.env.NEXT_PUBLIC_SPREADSHEET_ID;
-const SHEET_ID = process.env.NEXT_PUBLIC_SHEET_ID;
-const GOOGLE_CLIENT_EMAIL = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_EMAIL;
-const GOOGLE_SERVICE_PRIVATE_KEY =
-  process.env.NEXT_PUBLIC_GOOGLE_SERVICE_PRIVATE_KEY;
+
+const SHEET_ID = process.env.GOOGLE_SHEET_ID;
+
 
 const ContactForm = () => {
   const [form, setForm] = useState({
@@ -18,13 +17,13 @@ const ContactForm = () => {
     description: '',
   });
 
-  const doc = new GoogleSpreadsheet(SPREADSHEET_ID);
+  const doc = new GoogleSpreadsheet(process.env.GOOGLE_SPREADSHEET_ID);
 
   const appendSpreadsheet = async (row) => {
     try {
       await doc.useServiceAccountAuth({
-        client_email: GOOGLE_CLIENT_EMAIL,
-        private_key: GOOGLE_SERVICE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
       });
       // loads document properties and worksheets
       await doc.loadInfo();
