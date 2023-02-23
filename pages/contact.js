e61c05a7168991e9093020b95c25713fd8b9c8ab
Wendy Kelly <wendykelly@Wendys-iMac.local>
2023-02-23 06:50:51 -0800

    added replace to early

--- pages/contact.js
+++ pages/contact.js
@@ -5,7 +5,7 @@ const SPREADSHEET_ID = process.env.NEXT_PUBLIC_SPREADSHEET_ID;
 const SHEET_ID = process.env.NEXT_PUBLIC_SHEET_ID;
 const GOOGLE_CLIENT_EMAIL = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_EMAIL;
 const GOOGLE_SERVICE_PRIVATE_KEY =
-  process.env.NEXT_PUBLIC_GOOGLE_SERVICE_PRIVATE_KEY.replace(/\\n/g, '\n');
+  process.env.NEXT_PUBLIC_GOOGLE_SERVICE_PRIVATE_KEY;
 
 const ContactForm = () => {
   const [form, setForm] = useState({
