4e13aafa504debe6b9fccd01cd18c0f12759cdf3
Wendy Kelly <wendykelly@Wendys-iMac.local>
2023-02-24 13:56:27 -0800

    put POST back

--- pages/api/form.ts
+++ pages/api/form.ts
@@ -8,7 +8,9 @@ type SheetForm = {
 }
 export default async function handler(req: NextApiRequest,
   res: NextApiResponse){
-
+  if (req.method !== 'POST') {
+      return res.status(405).send({ message: 'Only POST requests allowed' })
+  }
 
   const body = req.body as SheetForm
 console.log(body)
