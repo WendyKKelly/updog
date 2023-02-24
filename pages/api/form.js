9e6c2c5ebe8cc15f3c3d6a56c984261b5b9d3439
Wendy Kelly <wendykelly@Wendys-iMac.local>
2023-02-24 06:57:45 -0800

    take out awaits

--- pages/api/form.js
+++ pages/api/form.js
@@ -3,14 +3,14 @@ import { GoogleSpreadsheet } from 'google-spreadsheet';
 
 export default async function handler(req, res) {
 if (req.method === 'POST') {
-await doc.useServiceAccountAuth({
+ doc.useServiceAccountAuth({
     client_email: process.env.NEXT_PUBLIC_GOOGLE_SERVICE_ACCOUNT_EMAIL,
     private_key: process.env.NEXT_PUBLIC_GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
   });
-  const doc = new GoogleSpreadsheet(process.env.NEXT_PUBLIC_GOOGLE_SPREADSHEET_ID);
-  await doc.loadInfo();
+  const  doc = new GoogleSpreadsheet(process.env.NEXT_PUBLIC_GOOGLE_SPREADSHEET_ID);
+   doc.loadInfo();
   const sheet = doc.sheetsById[0];
-  await sheet.addRow(row)
+   sheet.addRow(row)
 if(
     form.name !== '' 
    
