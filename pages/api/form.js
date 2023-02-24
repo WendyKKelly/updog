f2df611e586fef0f5ab84656ead7aa082d83a573
Wendy Kelly <wendykelly@Wendys-iMac.local>
2023-02-23 18:04:18 -0800

    try againa

--- pages/api/form.js
+++ pages/api/form.js
@@ -3,8 +3,6 @@ import { GoogleSpreadsheet } from 'google-spreadsheet';
 
 export default async function handler(req, res) {
 if (req.method === 'POST') {
-try {
-
 await doc.useServiceAccountAuth({
     client_email: process.env.NEXT_PUBLIC_GOOGLE_SERVICE_ACCOUNT_EMAIL,
     private_key: process.env.NEXT_PUBLIC_GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
@@ -13,13 +11,6 @@ await doc.useServiceAccountAuth({
   await doc.loadInfo();
   const sheet = doc.sheetsById[0];
   await sheet.addRow(row)
- return res.send(sheet)
-} catch (e) {
-  console.error('Error: ', e);
-}
-
-};
-const Append = () => {
 if(
     form.name !== '' 
    
@@ -28,9 +19,9 @@ if(
       FullName: form.name,
    
     };
-      Append(newRow);
+      sheet(newRow);
   }
-  res.status(200).json({ name: 'John Doe' })
-
+  return res.send(sheet)
+  
+};
 } 
-}
