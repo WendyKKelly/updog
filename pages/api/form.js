23124234943e8329c179667a3091a34e41ee8221
Wendy Kelly <wendykelly@Wendys-iMac.local>
2023-02-24 07:08:40 -0800

    try again

--- pages/api/form.js
+++ pages/api/form.js
@@ -3,14 +3,18 @@ import { GoogleSpreadsheet } from 'google-spreadsheet';
 
 export default async function handler(req, res) {
 if (req.method === 'POST') {
- doc.useServiceAccountAuth({
+ try{
+  await doc.useServiceAccountAuth({
     client_email: process.env.NEXT_PUBLIC_GOOGLE_SERVICE_ACCOUNT_EMAIL,
     private_key: process.env.NEXT_PUBLIC_GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
   });
-  const  doc = new GoogleSpreadsheet(process.env.NEXT_PUBLIC_GOOGLE_SPREADSHEET_ID);
-   doc.loadInfo();
+   const  doc = new GoogleSpreadsheet(process.env.NEXT_PUBLIC_GOOGLE_SPREADSHEET_ID);
+   await doc.loadInfo();
   const sheet = doc.sheetsById[0];
-   sheet.addRow(row)
+   await sheet.addRow(row)
+  }
+  catch (e) {
+   console.error('Error: ', e);
 if(
     form.name !== '' 
    
@@ -24,6 +28,7 @@ if(
   
   res.status(201).json({ message: 'It works!', response });
   
-};
+  }
 res.status(200).json({ message: 'Hey!' });
 }
+};
