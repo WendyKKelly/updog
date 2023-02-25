48c8c866db4e052436f662f19ee9d82652ecd327
Wendy Kelly <wendykelly@Wendys-iMac.local>
2023-02-25 06:54:51 -0800

    accidentally got rid of entire snipcart api

--- pages/api/form.js
+++ pages/api/form.js
@@ -7,9 +7,16 @@ console.log('body: ', body)
  // Guard clause checks for first and last name,
   // and returns early if they are not found
 
-  
+  // Guard clause checks for first and last name,
+  // and returns early if they are not found
+  if (!body.first || !body.last) {
+    // Sends a HTTP bad request error code
+    return res.status(400).json({ data: 'First or last name not found' })
+  }
 
   // Found the name.
   // Sends a HTTP success code
   res.status(200).json({ data: `${body.first} ${body.last}` })
 }
+
+
