282daa532c63ab8a68bba44984d0f4072d7ce26a
Wendy Kelly <wendykelly@Wendys-iMac.local>
2023-02-25 12:12:04 -0800

    add phone

--- pages/contact.js
+++ pages/contact.js
@@ -12,6 +12,7 @@ export default function PageWithJSbasedForm() {
     // Get data from the form.
     const data = {
       fullname: event.target.fullname.value,
+      phone: event.target.phone.value,
       email: event.target.email.value,
     }
 
@@ -39,19 +40,22 @@ export default function PageWithJSbasedForm() {
     // Get the response data from server as JSON.
     // If server returns the name submitted, that means the form works.
     const result = await response.json()
-    alert(`Is this your full name and email address: ${result.data}`)
+    alert(`Is this your full name and email address and phone: ${result.data}`)
   }
   return (
     <Layout>
+      <section>
 <main>
   
     <div>
-      <h5>Hello - Please fill out this form</h5>
+      <h1>Hello - Please fill out this form</h1>
     </div>
     <div className={styles.grid}>
     <form className={styles.form}onSubmit={handleSubmit}>
       <label className={styles.label}htmlFor="fullname">Full Name</label>
       <input className={styles.fullname}type="text" id="fullname" name="fullname" required />
+      <label className={styles.label}htmlFor="phone">Phone</label>
+      <input className={styles.phone}type="text" id="phone" name="phone" required />
 
       <label className={styles.label}htmlFor="email">Email</label>
       <input className={styles.email}type="text" id="email" name="email" required />
@@ -60,6 +64,7 @@ export default function PageWithJSbasedForm() {
     </form>
     </div>
     </main>
+    </section>
     </Layout>
   )
 }
