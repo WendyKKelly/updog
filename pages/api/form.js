d48eb9852aba171d184d7df0b8a643ad7db7c9d2
Wendy Kelly <wendykelly@Wendys-iMac.local>
2023-02-25 06:47:14 -0800

    logs way its working form still shows 400

--- pages/api/form.js
+++ pages/api/form.js
@@ -1,5 +1,4 @@
 
-import {google} from "googleapis";
 
 export default async function handler(req, res){
 
@@ -7,10 +6,8 @@ const body = req.body
 console.log('body: ', body)
  // Guard clause checks for first and last name,
   // and returns early if they are not found
-  if (!body.first || !body.last) {
-    // Sends a HTTP bad request error code
-    return res.status(400).json({ data: 'First or last name not found' })
-  }
+
+  
 
   // Found the name.
   // Sends a HTTP success code
