e6873035b452803b511391410a494d84a2c8e85b
Wendy Kelly <wendykelly@Wendys-iMac.local>
2023-02-26 12:42:23 -0800

    attach css to nelson page

--- pages/nelson.js
+++ pages/nelson.js
@@ -11,7 +11,7 @@ import Layout from '../components/Layout';
 
 import { Grandstander } from '@next/font/google'
 import {Cormorant} from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import styles from '@/styles/Contact.module.css'
 
 import Link from 'next/link'
 
