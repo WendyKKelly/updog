77e77e0ffd8b772259225bf6fb11698eb5376c54
Wendy Kelly <wendykelly@Wendys-iMac.local>
2023-03-05 14:10:09 -0800

    moved back to index

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,16 +8,8 @@ import type { AppProps } from 'next/app'
   return (<div className="app"> 
 
   <>
-   <Script id="smartsupp" >{`
-var _smartsupp = _smartsupp || {};
-_smartsupp.key = 'c1222087171f62064976d0dc7b117d05679bc47f';
-window.smartsupp||(function(d) {
-var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
-s=d.getElementsByTagName('script')[0];c=d.createElement('script');
-c.type='text/javascript';c.charset='utf-8';c.async=true;
-c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
-})(document);`}
-</Script>
+
+
   <Component {...pageProps} />
   </>
   </div>
