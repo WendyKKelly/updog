5e705034ba1b1b0340d198250fb8709eea555c8e
Wendy Kelly <wendykelly@Wendys-iMac.local>
2023-03-07 10:33:01 -0800

    take out other stuff as essential

--- pages/api/form.js
+++ pages/api/form.js
@@ -7,7 +7,7 @@ async function handler(req, res) {
   // Optional logging to see the responses
   // in the command line where next.js app is running.
   console.log('body: ', body)
-  if (!body.fullname || !body.tel || !body.email || !body.address ||!body.treats ||!body.sample ||!body.pouch || !body.order)  {
+  if (!body.fullname || !body.tel || !body.email || !body.address)  {
     // Sends a HTTP bad request error code
     return res.status(400).json({ data: 'Fullname or phone or email or address not found' })
   }
