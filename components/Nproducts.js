5ed2fc8bc04dee0bb39121d62f5677d46880c4ce
Wendy Kelly <wendykelly@Wendys-iMac.local>
2023-03-07 12:28:58 -0800

    fix css on nelson page

--- components/Nproducts.js
+++ components/Nproducts.js
@@ -3,7 +3,7 @@ import updogSample from "../public/images/samples.jpg"
 import updogTreat from "../public/images/120-grams.jpg"
 import updogPouch from "../public/images/treatpouch.jpg"
 import updogRefill from "../public/images/refill.png"
-import styles from '../styles/Product.module.css';
+import styles from '../styles/Nproduct.module.css';
 import { Grandstander } from '@next/font/google'
 import {Cormorant} from '@next/font/google'
 const grandstander = Grandstander({ subsets: ['latin'] })
@@ -16,10 +16,10 @@ export default function Nproducts () {
             <main className={styles.main}>
                 
         <div className={styles.product}>
-           <div className={styles.title}>
+           <div className={styles.t_title}>
             <div className={grandstander.className}>
             <h2 className={styles.title}>Updog Treats / 120 grams</h2></div></div>
-            <div className={styles.image}>
+            <div className={styles.t_image}>
             <Image 
             src={updogTreat} 
             alt={updogTreat}
@@ -27,20 +27,21 @@ export default function Nproducts () {
             height={300}
             priority />
             </div>
-            <div className={styles.desc}>
+            <div className={styles.t_desc}>
             <div className={cormorant.className}>
             <p className={styles.description}>120 grams of treats that will make your dog love you (even more). These treats are made with spent barley grain from a local brewery, fresh ground meat (mainly pork but may include beef and a small amount of chicken) and chia seeds. Best refrigerated but can last up to 10 days in the pantry.</p></div></div>
-            <div className={styles.ppb}>
+            <div className={styles.t_ppb}>
             <div className={cormorant.className}>
-                <div className={styles.price}>$10</div>
+                <div className={styles.t_button}>Order Now</div>
+                <div className={styles.t_price}>$10</div>
                 
                 
             </div>
         </div>
-        <div className={styles.title}>
+        <div className={styles.s_title}>
             <div className={grandstander.className}>
             <h2 className={styles.title}>Updog Sample</h2></div></div>
-            <div className={styles.image}>
+            <div className={styles.s_image}>
             <Image 
             src={updogSample} 
             alt={updogSample}
@@ -48,20 +49,21 @@ export default function Nproducts () {
             height={300}
             priority />
             </div>
-            <div className={styles.desc}>
+            <div className={styles.s_desc}>
             <div className={cormorant.className}>
             <p className={styles.description}>Curious? Order a small 10 gram sample of our delicious treats. So far, every dog we have given samples to has begged us for more — but we fully understand that you might want to try them out first. All our packaging is made inhouse with upcycled materials and we plan to keep it that way.</p></div></div>
-            <div className={styles.ppb}>
+            <div className={styles.s_ppb}>
             <div className={cormorant.className}>
-                <div className={styles.price}>$1</div>
+            <div className={styles.t_button}>Order Now</div>
+                <div className={styles.t_price}>$1</div>
                 
                 
             </div>
         </div>
-        <div className={styles.title}>
+        <div className={styles.p_title}>
             <div className={grandstander.className}>
             <h2 className={styles.title}>Updog Treat Pouch</h2></div></div>
-            <div className={styles.image}>
+            <div className={styles.p_image}>
             <Image 
             src={updogPouch} 
             alt={updogPouch}
@@ -69,20 +71,20 @@ export default function Nproducts () {
             height={300}
             priority />
             </div>
-            <div className={styles.desc}>
+            <div className={styles.p_desc}>
             <div className={cormorant.className}>
             <p className={styles.description}>Ultralight, upcycled, handmade with care from malt and grain bags. When brewmaster Mike made an offhand remark about upcycling grain bags, who could have predicted this? These treat bags now come in 2 sizes — small, about the size of a rock climber's chalk bag, and a larger one, about twice as large. No two are the same.</p></div></div>
-            <div className={styles.ppb}>
+            <div className={styles.p_ppb}>
             <div className={cormorant.className}>
-                <div className={styles.price}>$10</div>
-                
+                <div className={styles.t_price}>$10</div>
+                <div className={styles.t_button}>Order Now</div>
                 
             </div>
         </div>
-        <div className={styles.title}>
+        <div className={styles.tp_title}>
             <div className={grandstander.className}>
             <h2 className={styles.title}>Updog Treat Pouch w/ 120 grams treats</h2></div></div>
-            <div className={styles.image}>
+            <div className={styles.tp_image}>
             <Image 
             src={updogRefill} 
             alt={updogRefill}
@@ -90,13 +92,13 @@ export default function Nproducts () {
             height={300}
             priority />
             </div>
-            <div className={styles.desc}>
+            <div className={styles.tp_desc}>
             <div className={cormorant.className}>
             <p className={styles.description}>Buy a handmade upcycled treat pouch (large or small) and save $1 on the treats, every time it's refilled. That's like a free bag of treats every 10 refills!</p></div></div>
-            <div className={styles.ppb}>
+            <div className={styles.tp_ppb}>
             <div className={cormorant.className}>
-                <div className={styles.price}>$19</div>
-                
+                <div className={styles.t_price}>$19</div>
+                <div className={styles.t_button}>Order Now</div>
                 
             </div>
         </div>
