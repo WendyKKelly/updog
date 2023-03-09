582728f2dff7c0d0abe06d168f467d858c8bdec8
Wendy Kelly <wendykelly@Wendys-iMac.local>
2023-03-09 10:47:03 -0800

    add new chat app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,19 @@ import type { AppProps } from 'next/app'
 
 
   <Component {...pageProps} />
+ 
+<Script type="text/javascript">
+{`var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/640a28844247f20fefe4faef/1gr3qshe1';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();`}
+</Script>
+
   </>
   </div>
   )
