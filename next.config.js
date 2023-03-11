b8b788108135a581cdbacd7b669df7d12d719081
Wendy Kelly <wendykelly@Wendys-iMac.local>
2023-03-11 13:15:17 -0800

    new sitemap based on next docs

--- next.config.js
+++ next.config.js
@@ -1,11 +1,26 @@
 module.exports = {
+  async rewrites() {
+
+    return [
+
+      {
+
+        source: '/sitemap.xml',
+
+        destination: '/api/sitemap',
+
+      },
+
+    ]
+
+  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback.fs = false;
       config.resolve.fallback.tls = false;
       config.resolve.fallback.net = false;
       config.resolve.fallback.child_process = false;
-      require(".scripts/sitemap-generator");
+     
     }
 
     return config;
