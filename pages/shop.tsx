592f24f781aa56bb2973929bf1119a751b5d1128
Wendy Kelly <wendykelly@Wendys-iMac.local>
2023-03-15 16:53:58 -0700

    add indes to api to check json

--- pages/shop.tsx
+++ pages/shop.tsx
@@ -37,6 +37,9 @@ export default function Shop({products}: IProductListProps){
   content="/images/updoglogogold.png"
 />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
+<link rel="preconnect" href="https://app.snipcart.com"/>
+                <link rel="preconnect" href="https://cdn.snipcart.com"/>
+                <link rel="stylesheet" href="https://cdn.snipcart.com/themes/v3.2.0/default/snipcart.css"/>
 <link rel="icon" href="/favicon.ico" />
 
 
@@ -67,7 +70,7 @@ export const products: IProduct[] = [
       image: updogTreat,
       description: "120 grams of treats that will make your dog love you (even more). These treats are made with spent barley grain from a local brewery, fresh ground meat (mainly pork but may include beef and a small amount of chicken) and chia seeds. Best refrigerated but can last up to 10 days in the pantry."
         ,
-      url: './api/products/treats',
+      url: '/api/products/treats',
       weight: 120,
       length: 10,
       width: 5,
@@ -79,7 +82,7 @@ export const products: IProduct[] = [
         price: 1.00,
         image: updogSample,
         description: "Curious? Order a small 10 gram sample of our delicious treats. So far, every dog we have given samples to has begged us for more :) but we fully understand that you might want to try them out first. All our packaging is made inhouse with upcycled materials and we plan to keep it that way.",
-        url: './api/products/sample',
+        url: '/api/products/sample',
         weight: 120,
         length: 10,
         width: 5,
@@ -92,7 +95,7 @@ export const products: IProduct[] = [
         price: 10.00,
         image: updogPouch,
         description: "Ultralight, upcycled, handmade with care from malt and grain bags. When brewmaster Mike made an offhand remark about upcycling grain bags, who could have predicted this? These treat bags now come in 2 sizes — small, about the size of a rock climber's chalk bag, and a larger one, about twice as large. No two are the same.",
-        url: './api/products/pouch',
+        url: '/api/products/pouch',
         weight: 120,
         length: 10,
         width: 5,
@@ -104,7 +107,7 @@ export const products: IProduct[] = [
         price: 19.00,
         image: updogRefill,
         description: "Buy a handmade upcycled treat pouch (large or small) and save $1 on the treats, every time it's refilled. That's like a free bag of treats every 10 refills!",
-        url: './api/products/refill',
+        url: '/api/products/refill',
         weight: 120,
         length: 10,
         width: 5,
