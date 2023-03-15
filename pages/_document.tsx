0a52962244b13ff2e3d2e4494336e8bec7e1f8a2
Wendy Kelly <wendykelly@Wendys-iMac.local>
2023-03-15 12:04:26 -0700

    try again with install in shop

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document(){
   
   return (
+    
     <Html lang="en">
+    
       <Head> 
                 <link rel="shortcut icon" href="../favicon.ico" />
                 </Head>
@@ -11,16 +13,11 @@ export default function Document(){
     
         <Main />
         <NextScript />
-        <script async 
-        src="https://cdn.snipcart.com/themes/v3.4.1/default/snipcart.js"></script>
-  <div
-  id= "snipcart"
-  data-config-modal-style= "side"
-  data-api-key={`${process.env.NEXT_PUBLIC_SNIPCART_APII_KEY}`}
-  hidden ></div>
+
   
       </body>
     </Html>
+    
   )
 }
 
