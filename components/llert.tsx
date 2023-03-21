75ee335c4b4621c55be48d412a5b5388fc603174
Wendy Kelly <wendykelly@Wendys-iMac.local>
2023-03-21 10:53:48 -0700

    style blog alert

--- components/llert.tsx
+++ components/llert.tsx
@@ -1,4 +1,4 @@
-import Lontainer from './lontainer'
+
 import Link from 'next/link'
 import styles from "@/styles/Llert.module.css";
 import { Grandstander } from "@next/font/google";
@@ -7,18 +7,16 @@ const grandstander = Grandstander({ subsets: ['latin'] })
 export default function Llert() {
   return (
    
-      <Lontainer>
-        <div className={styles.description}>
+      <div className={styles.container}>
          
             <>
             <Link 
         href="./shop"><div className={grandstander.className}><p className={styles.description}>Free local delivery</p></div>
         </Link>
-              .
             </>
           
         </div>
-      </Lontainer>
+     
     
   )
 }
